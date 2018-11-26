1735f550dac26e3417d10daffd172d9bbcad1565
EmmanuelTef123 <eteferi@princeton.edu>
2018-11-25 21:47:23 -0500

    First commit

--- app/src/main/java/com/emmanuel/map2/MapsActivity.java
+++ app/src/main/java/com/emmanuel/map2/MapsActivity.java
@@ -1,30 +1,97 @@
-
 package com.emmanuel.map2;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+/*import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.firebase.database.ChildEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener; */
+
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+
+import android.location.LocationManager;
+import android.location.Location;
+import android.location.Criteria;
+import android.content.Context;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+import android.content.Intent;
+import java.net.URL;
+import android.os.AsyncTask;
+import android.view.View;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import android.util.Log;
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONTokener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.app.Activity;
+
+
+
+public class MapsActivity extends FragmentActivity implements  OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final int LOCATION_REQUEST = 500;
+    EditText emailText;
+    TextView responseView;
+    ProgressBar progressBar;
+    String address;
+    static final String API_URL_FORWARD = "https://api.what3words.com/v2/forward?";
+    static final String API_URL_REVERSE = "https://api.what3words.com/v2/reverse?";
+    private static final String LOG_TAG =
+            MapsActivity.class.getSimpleName();
+    Intent intentThatCalled;
+    public double latitude;
+    public double longitude;
+    public LocationManager locationManager;
+    public Criteria criteria;
+    public String bestProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, " onReady!!");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map1);
-        mapFragment.getMapAsync(this);
-    }
 
+        Button queryButton = (Button) findViewById(R.id.queryButton);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment1 = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map1);
+        mapFragment1.getMapAsync(this);
+
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,11 +104,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng oldNassau = new LatLng(40.3487, -74.6593);
-        mMap.addMarker(new MarkerOptions().position(oldNassau).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(oldNassau));
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+            return;
+        }
+        //GET https://api.what3words.com/v2/forward?addr=index.home.raft&display=full&format=json&key=[API-KEY]
+
+        //mMap.setMyLocationEnabled(true);
+
+        // locationManager = (LocationManager)  this.getSystemService(Context.LOCATION_SERVICE);
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case LOCATION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                }
+                break;
+        }
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button Clicked!!");
+        Intent intent = new Intent(this, StorageActivity.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/emmanuel/map2/StorageActivity.java
+++ app/src/main/java/com/emmanuel/map2/StorageActivity.java
@@ -11,16 +11,22 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.apache.http.conn.ssl.StrictHostnameVerifier;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 
 public class StorageActivity extends AppCompatActivity {
 
@@ -28,23 +34,9 @@ public class StorageActivity extends AppCompatActivity {
     TextView responseView;
     ProgressBar progressBar;
     String address;
-    static final String API_KEY = "04XGAW07";
     static final String API_URL_FORWARD = "https://api.what3words.com/v2/forward?";
     static final String API_URL_REVERSE = "https://api.what3words.com/v2/reverse?";
-/*
-    class Point {
 
-        public double lng;
-        public double lat;
-        public String w3w;
-
-        public Point(double lat, double lng, String w3w) {
-            this.lng = lng;
-            this.lat = lat;
-            this.w3w = w3w;
-        }
-    }
-*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +47,7 @@ public class StorageActivity extends AppCompatActivity {
         queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new StorageActivity.RetrieveWordsTask().execute();
+                new StorageActivity.RetrievePositionTask().execute();
             }
         });
     }
@@ -64,16 +56,30 @@ public class StorageActivity extends AppCompatActivity {
 
         private Exception exception;
 
+        HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                HostnameVerifier hv = new StrictHostnameVerifier();
+
+                return hv.verify("example.com", session);
+            }
+        };
+
         protected String doInBackground(Void... urls) {
             // String address = (String) findViewById(R.id.str).getText().toString();
             // Do some validation here
 
             try {
                 final EditText edit =  (EditText) findViewById(R.id.str);
-                address = edit.getText().toString();
-                URL url = new URL(API_URL_FORWARD + "addr=" + address + "&key=" + API_KEY);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
+                    address = edit.getText().toString();
+                    URL url = new URL(API_URL_FORWARD + "addr=" + address + "&key=" + API_KEY);
+                    Log.d("here", "error*");
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    //urlConnection.setHostnameVerifier(hostnameVerifier);
+                    int length = urlConnection.getContentLength();
+                    InputStream in = urlConnection.getInputStream();
+
+                    try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
                     StringBuilder stringBuilder = new StringBuilder();
                     String line;
@@ -102,11 +108,11 @@ public class StorageActivity extends AppCompatActivity {
             try {
 
                 JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-                //String geometry = object.getString("geometry");
-
                 String geometry = object.getString("geometry");
-                Log.i("geometry", geometry);
                 responseView.setText(geometry);
+
+              //  String geometry = object.getJSONObject("geometry");
+              //  Log.i("geometry", geometry);
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -114,19 +120,18 @@ public class StorageActivity extends AppCompatActivity {
         }
     }
 
-
     class RetrieveWordsTask extends AsyncTask<Void, Void, String> {
 
         private Exception exception;
 
+
+
         protected String doInBackground(Void... urls) {
-            // String address = (String) findViewById(R.id.str).getText().toString();
-            // Do some validation here
+            final EditText edit =  (EditText) findViewById(R.id.str);
+            address = edit.getText().toString();
 
             try {
-                final EditText edit =  (EditText) findViewById(R.id.str);
-                String coords = edit.getText().toString();
-                URL url = new URL(API_URL_REVERSE + "coords=" + coords + "&key=" + API_KEY);
+                URL url = new URL(API_URL_REVERSE + "addr=" + address + "&key=" + API_KEY);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -137,36 +142,28 @@ public class StorageActivity extends AppCompatActivity {
                     }
                     bufferedReader.close();
                     return stringBuilder.toString();
-                } finally {
+                }
+                finally{
                     urlConnection.disconnect();
                 }
-            } catch (Exception e) {
+            }
+            catch(Exception e) {
                 Log.e("ERROR", e.getMessage(), e);
                 return null;
             }
         }
 
         protected void onPostExecute(String response) {
-            if (response == null) {
+            if(response == null) {
                 response = "THERE WAS AN ERROR";
             }
-            TextView responseView = (TextView) findViewById(R.id.responseView);
-
-            try {
-
-                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-                String words = object.getString("words");
-                Log.i("INFO", words);
-                responseView.setText(words);
-
-
-
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            Log.i("INFO", response);
+            responseView.setText(response);
         }
     }
+
+
+
 
 
 
